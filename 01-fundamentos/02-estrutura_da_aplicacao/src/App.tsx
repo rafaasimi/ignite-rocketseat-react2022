f98fbed51329038573f91e0adf23132e46d85b21
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-03-18 20:37:39 -0300

    aula: add sidebar

--- 01-fundamentos/02-estrutura_da_aplicacao/src/App.tsx
+++ 01-fundamentos/02-estrutura_da_aplicacao/src/App.tsx
@@ -2,20 +2,26 @@ import { Header } from "./components/Header";
 import { Post } from "./Post";
 
 import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
     <>
       <Header />
-      <h1>Hello World</h1>
-      <Post
-        author="Rafael Zorzenon"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-      />
-      <Post
-        author="Rafael Zorzenon 2"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit 2."
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Rafael Zorzenon"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          />
+          <Post
+            author="Rafael Zorzenon 2"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit 2."
+          />
+        </main>
+      </div>
     </>
   );
 }
