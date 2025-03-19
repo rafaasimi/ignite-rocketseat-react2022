44757fcef5ce19e569d6edda759e1bea3a39cf14
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-03-19 08:18:38 -0300

    aula: add componente post

--- 01-fundamentos/02-estrutura_da_aplicacao/src/App.tsx
+++ 01-fundamentos/02-estrutura_da_aplicacao/src/App.tsx
@@ -1,9 +1,9 @@
 import { Header } from "./components/Header";
-import { Post } from "./Post";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
 
 import "./global.css";
 import styles from "./App.module.css";
-import { Sidebar } from "./components/Sidebar";
 
 function App() {
   return (
@@ -12,14 +12,10 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Rafael Zorzenon"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          />
-          <Post
-            author="Rafael Zorzenon 2"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit 2."
-          />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
         </main>
       </div>
     </>
--- 01-fundamentos/02-estrutura_da_aplicacao/src/Post.tsx
+++
@@ -1,8 +0,0 @@
-export function Post(props) {
-  return (
-    <>
-      <h3>{props.author}</h3>
-      <p>{props.content}</p>
-    </>
-  );
-}
