5ac1ed367e2467ad7f5b0db53f2061167fbd6249
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-03-19 14:52:57 -0300

    aula: iteração e propriedades nos posts

---
+++ 01-fundamentos/03-os_motores_do_react/src/App.tsx
@@ -0,0 +1,125 @@
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
+
+import "./global.css";
+import styles from "./App.module.css";
+
+// author: { avatar_url: "", name: "", role: "" }
+// publishedAt: Date
+// content: String
+
+export interface IPost {
+  id: number;
+  author: {
+    avatarUrl: string;
+    name: string;
+    role: string;
+  };
+  content: {
+    type: string;
+    content: string;
+  }[];
+  publishedAt: Date;
+}
+
+const posts: IPost[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "http://github.com/rafaasimi.png",
+      name: "Rafael Simionato",
+      role: "Engenheiro de Software",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "👉 jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2025-03-19 14:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "http://github.com/diego3g.png",
+      name: "Diego Fernandes",
+      role: "CTO @Rocketseat",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "👉 jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2025-03-15 10:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "http://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Instrutor @Rocketseat",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galera! Mayk na área.",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Saiu conteúdo novo no canal... Bora conferir?",
+      },
+      {
+        type: "link",
+        content: "👉 youtube.com/maykbrito",
+      },
+    ],
+    publishedAt: new Date("2025-02-20 10:00:00"),
+  },
+];
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                id={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
