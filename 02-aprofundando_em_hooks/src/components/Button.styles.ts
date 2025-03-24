d0480bcce8e7fe8f4540c05370c81a0f318978bf
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-03-24 19:22:31 -0300

    aula: configurando temas

--- 02-aprofundando_em_hooks/src/components/Button.styles.ts
+++ 02-aprofundando_em_hooks/src/components/Button.styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export type ButtonVariant = "primary" | "secondary" | "success" | "danger";
 
@@ -17,7 +17,12 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
-  ${(props) => {
-    return `background-color: ${buttonVariants[props.variant]};`;
-  }}
+  background: ${(props) => props.theme.primary};
+  color: ${(props) => props.theme.white};
+
+  /* ${(props) => {
+    return css`
+      background-color: ${buttonVariants[props.variant]};
+    `;
+  }} */
 `;
