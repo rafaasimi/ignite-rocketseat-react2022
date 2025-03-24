b178fe5108454db1813505ab8a8e7cd2ee08093d
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-03-24 17:18:03 -0300

    add styled components

--- 02-aprofundando_em_hooks/src/App.tsx
+++ 02-aprofundando_em_hooks/src/App.tsx
@@ -1,3 +1,13 @@
+import { Button } from "./components/Button";
+
 export function App() {
-  return <>Hello World</>;
+  return (
+    <>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </>
+  );
 }
