d3c372cd6d944180954a83fcbb5b12981af73513
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-03-25 10:27:13 -0300

    aula: finalização da home

--- 02-aprofundando_em_hooks/src/pages/Home/index.tsx
+++ 02-aprofundando_em_hooks/src/pages/Home/index.tsx
@@ -1,5 +1,13 @@
 import { Play } from "phosphor-react";
-import { CountdownContainer, FormContainer, HomeContainer, Separador } from "./styles";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separador,
+  StartCountdownButton,
+  TaskInput,
+} from "./styles";
 
 export function Home() {
   return (
@@ -7,10 +15,18 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input type="text" id="task" />
+          <TaskInput
+            type="text"
+            id="task"
+            placeholder="Dê um nome para o seu projeto"
+          />
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+          />
           <span>minutos.</span>
         </FormContainer>
 
@@ -22,9 +38,9 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCountdownButton type="submit" disabled>
           <Play size={24} /> Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   );
