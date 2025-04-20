3ed9cce988871deb45528a20b04684d2a4f20b34
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-04-20 10:04:13 -0300

    aula: botões de entrada e saída

--- 03-http_e_performance/src/components/NewTransactionModal/index.tsx
+++ 03-http_e_performance/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { CloseButton, Content, Overlay } from "./styles";
-import { X } from "phosphor-react";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./styles";
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 
 export function NewTransactionModal() {
   return (
@@ -14,12 +20,23 @@ export function NewTransactionModal() {
 
         <form action="">
           <input type="text" placeholder="Descrição" required />
-          <input type="number" placeholder="Preço" required/>
-          <input type="text" placeholder="Categoria" required/>
+          <input type="number" placeholder="Preço" required />
+          <input type="text" placeholder="Categoria" required />
+
+          <TransactionType>
+            <TransactionTypeButton variant="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
 
           <button type="submit">Cadastrar</button>
         </form>
-
       </Content>
     </Dialog.Portal>
   );
--- 03-http_e_performance/src/components/NewTransactionModal/styles.ts
+++ 03-http_e_performance/src/components/NewTransactionModal/styles.ts
@@ -49,7 +49,7 @@ export const Content = styled(Dialog.Content)`
       border-radius: 6px;
       margin-top: 1.5rem;
       cursor: pointer;
-      
+
       &:hover {
         background: ${(props) => props.theme["green-700"]};
         transition: background-color 0.2s;
@@ -67,4 +67,35 @@ export const CloseButton = styled(Dialog.Close)`
   line-height: 0;
   cursor: pointer;
   color: ${(props) => props.theme["gray-500"]};
+`;
+
+export const TransactionType = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 0.5rem;
+  margin-top: 0.5rem;
+`;
+
+interface TransactionTypeButtonProps {
+  variant: "income" | "outcome";
+}
+
+export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+  background: ${(props) => props.theme["gray-700"]};
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  border-radius: 6px;
+  cursor: pointer;
+  border: 0;
+  color: ${(props) => props.theme["gray-300"]};
+
+  svg {
+    color: ${(props) =>
+      props.variant === "income"
+        ? props.theme["green-300"]
+        : props.theme["red-300"]};
+  }
 `;
