c69e8e463f7dd810c8726cace97ca71b6cdc993d
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-04-24 13:44:17 -0300

    aula: instalando shadcn/ui

--- 04-integrando_frontend_backend/src/App.tsx
+++ 04-integrando_frontend_backend/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+import "./styles/global.css";
+
 export function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="flex items-center justify-center h-screen">
+      <Button>Enviar</Button>
+    </div>
   );
 }
