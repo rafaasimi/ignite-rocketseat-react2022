9e7e46170b6860473b60adf5084fcbca41fc55f7
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-04-25 11:22:16 -0300

    aula: página cadastro de restaurante

--- 04-integrando_frontend_backend/src/pages/auth/sign-in.tsx
+++ 04-integrando_frontend_backend/src/pages/auth/sign-in.tsx
@@ -3,6 +3,7 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
 import { useForm } from 'react-hook-form'
+import { Link } from 'react-router'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
@@ -40,7 +41,12 @@ export function SignIn() {
   return (
     <>
       <title>Login | pizza.shop</title>
+
       <div className="p-8">
+        <Button variant="ghost" asChild className="absolute top-8 right-8">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
