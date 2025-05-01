a1a3420ae3332ae4325fb79b328d5b6dcb184690
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-05-01 18:38:41 -0300

    aula: configurando api client

--- 04-integrando_frontend_backend/pizzashop-front/src/App.tsx
+++ 04-integrando_frontend_backend/pizzashop-front/src/App.tsx
@@ -6,13 +6,18 @@ import './styles/global.css'
 import { router } from './routes'
 import { ThemeProvider } from './components/theme/theme-provider'
 
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
+
 export function App() {
   return (
     <>
       <title>Pizza Shop</title>
       <ThemeProvider defaultTheme="system" storageKey="pizzashop-theme">
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   )
