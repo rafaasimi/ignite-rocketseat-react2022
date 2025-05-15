ae5fa2c228853f7c690935bdc4d0b8dc7251e047
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-05-15 13:45:09 -0300

    aula: loading do detalhe do pedido

--- 04-integrando_frontend_backend/pizzashop-web/src/pages/app/orders/order-details.tsx
+++ 04-integrando_frontend_backend/pizzashop-web/src/pages/app/orders/order-details.tsx
@@ -14,6 +14,7 @@ import { DialogDescription, DialogTitle } from '@radix-ui/react-dialog'
 import { useQuery } from '@tanstack/react-query'
 import { formatDistanceToNow } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
+import { OrderDetailsSkeleton } from './order-details-skeleton'
 
 export interface OrderDetailsProps {
   orderId: string
@@ -36,7 +37,7 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
         <DialogDescription>Detalhes do pedido</DialogDescription>
       </DialogHeader>
 
-      {order && (
+      {order ? (
         <div className="space-y-6">
           <Table>
             <TableBody>
@@ -132,6 +133,8 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
             </TableFooter>
           </Table>
         </div>
+      ) : (
+        <OrderDetailsSkeleton />
       )}
     </DialogContent>
   )
