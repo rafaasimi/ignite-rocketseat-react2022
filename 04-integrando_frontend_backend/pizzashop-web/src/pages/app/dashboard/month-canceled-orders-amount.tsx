6a3ee2c1056deec6bb9768b7edf81f6ed838fbde
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-05-15 09:11:00 -0300

    aula: loading das metricas

--- 04-integrando_frontend_backend/pizzashop-web/src/pages/app/dashboard/month-canceled-orders-amount.tsx
+++ 04-integrando_frontend_backend/pizzashop-web/src/pages/app/dashboard/month-canceled-orders-amount.tsx
@@ -2,6 +2,7 @@ import { getMonthCanceledOrdersAmount } from '@/api/get-month-cancelled-orders-a
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
 import { useQuery } from '@tanstack/react-query'
 import { DollarSign } from 'lucide-react'
+import { MetricCardSkeleton } from './metric-card-skeleton'
 
 export function MonthCanceledOrdersAmount() {
   const { data: monthCanceledOrdersAmount } = useQuery({
@@ -18,7 +19,7 @@ export function MonthCanceledOrdersAmount() {
         <DollarSign className="text-muted-foreground h-4 w-4" />
       </CardHeader>
       <CardContent className="space-y-1">
-        {monthCanceledOrdersAmount && (
+        {monthCanceledOrdersAmount ? (
           <>
             <span className="text-2xl font-bold tracking-tight">
               {monthCanceledOrdersAmount.amount.toLocaleString('pt-BR')}
@@ -41,6 +42,8 @@ export function MonthCanceledOrdersAmount() {
               )}
             </p>
           </>
+        ) : (
+          <MetricCardSkeleton />
         )}
       </CardContent>
     </Card>
