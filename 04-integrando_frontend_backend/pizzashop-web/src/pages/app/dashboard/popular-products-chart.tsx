0d276d4239477f0aac8b60bec85f81dc15bcbd6d
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-05-15 09:16:14 -0300

    aula: loading dos gráficos

--- 04-integrando_frontend_backend/pizzashop-web/src/pages/app/dashboard/popular-products-chart.tsx
+++ 04-integrando_frontend_backend/pizzashop-web/src/pages/app/dashboard/popular-products-chart.tsx
@@ -1,7 +1,7 @@
 import colors from 'tailwindcss/colors'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { ResponsiveContainer, PieChart, Pie, Tooltip, Cell } from 'recharts'
-import { BarChart } from 'lucide-react'
+import { BarChart, Loader2 } from 'lucide-react'
 import { getPopularProducts } from '@/api/get-popular-products'
 import { useQuery } from '@tanstack/react-query'
 
@@ -30,7 +30,7 @@ export function PopularProductsChart() {
         </div>
       </CardHeader>
       <CardContent>
-        {popularProducts && (
+        {popularProducts ? (
           <ResponsiveContainer width="100%" height={240}>
             <PieChart style={{ fontSize: 12 }}>
               <Pie
@@ -87,6 +87,10 @@ export function PopularProductsChart() {
               </Pie>
             </PieChart>
           </ResponsiveContainer>
+        ) : (
+          <div className='flex h-[240px] w-full items-center justify-center'>
+            <Loader2 className='h-8 w-8 text-muted-foreground animate-spin' />
+          </div>
         )}
       </CardContent>
     </Card>
